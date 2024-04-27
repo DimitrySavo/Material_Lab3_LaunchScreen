88c29ddb6be2b09eb9ea08d41cdab28204cfa21b
DimitryS <dimitrysavo@gmail.com>
2024-04-27 12:40:09 +0300

    Create Login activity

--- app/src/main/java/com/example/material_lab3_launchscreen/MainActivity.kt
+++ app/src/main/java/com/example/material_lab3_launchscreen/MainActivity.kt
@@ -11,14 +11,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-        installSplashScreen()
-
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
     }
 }
